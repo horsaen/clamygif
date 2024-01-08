5009e07e8fb3654b814e421418da3a1e14eda50f
horsaen <atealltheglue@protonmail.com>
2024-01-08 14:44:12 -0600

    small updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/navbar/Navbar";
+import { Metadata } from "next";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 import { GiOysterPearl } from "react-icons/gi";
 
 import styles from './page.module.css'
+
+export const metadata: Metadata = {
+  title: 'Clamygif',
+  description: 'A sleek way to store your files.',
+}
 
 export default async function Home() {
   const session: any = await getServerSession(authOptions);
